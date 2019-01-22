e28cc297da44d146656f4fe50eb8c6820ee77bdf
Sam Knoll <sknoll@shipleyschool.org>
2019-01-22 08:52:59 -0500

    commiting my current changes. Still working on reading in the CSV file

--- CommChoice.js
+++ CommChoice.js
@@ -1,40 +1,51 @@
-function CellChoice(){
+function handleFiles() {
+  // Check for the various File API support.
+  if (window.FileReader) {
+      // FileReader are supported.
+      getAsText("file:///UF.csv");
+  } else {
+      alert('FileReader are not supported in this browser.');
+  }
+}
 
-    var piece = document.getElementsByName('PieceType');
+function getAsText(fileToRead) {
+  var reader = new FileReader();
+  // Read file into memory as UTF-8
+  reader.readAsText(fileToRead);
+  alert(reader.readAsText(fileToRead));
 
-    var x = document.getElementById("frm");
-    var text = "";
-    var i;
-    for (i = 7; i < x.length-1 ;i++) {
-        text += x.elements[i].value+" ";
+  // Handle errors load
+  reader.onload = loadHandler;
+  reader.onerror = errorHandler;
+}
+
+function loadHandler(event) {
+  var csv = event.target.result;
+  processData(csv);
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    for (var i=0; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(';');
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
     }
-    alert(text);
-    var columns = [
-  [UBR,C],
-  [UBL,B],
-  //add more columns
-  //then add rows
-];
 }
-var excelIO = new GC.Spread.Excel.IO();
-var excelUrl = $("#importUrl").val();
-function ImportFile() {
-    var excelUrl = "./Full 3Style Comm Series + more.xlsx";
 
-    var oReq = new XMLHttpRequest();
-    oReq.open('get', excelUrl, true);
-    oReq.responseType = 'blob';
-    oReq.onload = function () {
-        var blob = oReq.response;
-        excelIO.open(blob, LoadSpread, function (message) {
-            console.log(message);
-        });
-    };
-    oReq.send(null);
+function errorHandler(evt) {
+  if(evt.target.error.name == "NotReadableError") {
+      alert("Canno't read file !");
+  }
 }
-function LoadSpread(json) {
-    jsonData = json;
-    workbook.fromJSON(json);
 
-    workbook.setActiveSheet("Revenues (Sales)");
+function commChoice(PieceOne,PieceTwo, PieceThree){
+ if(PieceOne=="UF" && PieceTwo=="UB" && PieceThree=="FR"){
+   alert("[R' U' : [R2, S]]");
+ }
+
 }
