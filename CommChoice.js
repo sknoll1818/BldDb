fef20c600d7b10e3b2a61bc5b7971bf0ed863e12
Sam Knoll <sknoll@shipleyschool.org>
2019-01-23 15:33:30 -0500

    working UF

--- CommChoice.js
+++ CommChoice.js
@@ -1,51 +1,102 @@
-function handleFiles() {
-  // Check for the various File API support.
-  if (window.FileReader) {
-      // FileReader are supported.
-      getAsText("file:///UF.csv");
-  } else {
-      alert('FileReader are not supported in this browser.');
+function cellChoice() {
+  var buffer = document.getElementById('buf').value;
+  var piece2 = document.getElementById('piece2').value;
+  var piece3 = document.getElementById('piece3').value;
+  var oTable = document.getElementById('UFTable');
+
+  var Row = document.getElementById(piece2);
+  var Cells = Row.getElementsByTagName('td');
+  var comm;
+  switch (piece3) {
+    case "UR":
+      comm = Cells[2].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "UB":
+      comm = Cells[1].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "UL":
+      comm = Cells[3].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FR":
+      comm = Cells[4].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FD":
+      comm = Cells[5].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "FL":
+      comm = Cells[6].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DF":
+      comm = Cells[7].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DR":
+      comm = Cells[8].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DB":
+      comm = Cells[8].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "DL":
+      comm = Cells[10].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RB":
+      comm = Cells[11].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RD":
+      comm = Cells[12].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RF":
+      comm = Cells[13].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "RU":
+      comm = Cells[14].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LB":
+      comm = Cells[15].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LU":
+      comm = Cells[16].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LF":
+      comm = Cells[17].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "LD":
+      comm = Cells[18].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BD":
+      comm = Cells[19].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BR":
+      comm = Cells[20].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BU":
+      comm = Cells[21].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
+    case "BL":
+      comm = Cells[22].innerHTML;
+      document.getElementById('Comm').innerHTML  = comm;
+      break;
   }
-}
-
-function getAsText(fileToRead) {
-  var reader = new FileReader();
-  // Read file into memory as UTF-8
-  reader.readAsText(fileToRead);
-  alert(reader.readAsText(fileToRead));
-
-  // Handle errors load
-  reader.onload = loadHandler;
-  reader.onerror = errorHandler;
-}
-
-function loadHandler(event) {
-  var csv = event.target.result;
-  processData(csv);
-}
-
-function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
-    var lines = [];
-    for (var i=0; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(';');
-            var tarr = [];
-            for (var j=0; j<data.length; j++) {
-                tarr.push(data[j]);
-            }
-            lines.push(tarr);
-    }
-}
-
-function errorHandler(evt) {
-  if(evt.target.error.name == "NotReadableError") {
-      alert("Canno't read file !");
-  }
-}
-
-function commChoice(PieceOne,PieceTwo, PieceThree){
- if(PieceOne=="UF" && PieceTwo=="UB" && PieceThree=="FR"){
-   alert("[R' U' : [R2, S]]");
- }
+  return null;
 
 }
